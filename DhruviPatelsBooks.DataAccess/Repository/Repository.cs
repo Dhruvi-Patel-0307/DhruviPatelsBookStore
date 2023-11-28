f4c88d199eec46dc9f14863ccc04b57aa40887c4
W0831844 <W0831844@ADL-A1201B-27>
2023-11-27 20:43:09 -0500

    create ISP_Call and SP_Call then modify them

--- DhruviPatelsBooks.DataAccess/Repository/Repository.cs
+++ DhruviPatelsBooks.DataAccess/Repository/Repository.cs
@@ -1,4 +1,6 @@
 ﻿using DhruviPatelsBooks.DataAccess.Repository.IRepository;
+using DhruviPatelsBookStore.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,36 +9,83 @@ using System.Text;
 
 namespace DhruviPatelsBooks.DataAccess.Repository
 {
+    
     public class Repository<T> : IRepository<T> where T : class
     {
+       
+        private readonly ApplicationDbContext _db;   
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)   
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);     
         }
 
         public T Get(int id)
         {
-            throw new NotImplementedException();
+            return dbSet.Find(id);
         }
 
         public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();     
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.FirstOrDefault();      
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            T entity = dbSet.Find(id);
+            Remove(entity);
         }
 
         public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<T> entity)
         {
-            throw new NotImplementedException();
+            dbSet.RemoveRange(entity);
         }
     }
 }
