0439f00aa1c3cc59dc74bb4cb14ff23aed0d20fe
W0831844 <W0831844@ADL-A1201B-27>
2023-11-27 20:48:41 -0500

    modify the UnitOfWork

---
+++ DhruviPatelsBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DhruviPatelsBooks.DataAccess.Repository.IRepository
+{
+    interface IUnitOfWork
+    {
+    }
+}
