d237ec257035ba079bba950e79837f095c6fd6dc
W0831844 <W0831844@ADL-A1201B-27>
2023-11-27 20:04:49 -0500

    add Admin and complete part 1

--- DhruviPatelsBookStore/Startup.cs
+++ DhruviPatelsBookStore/Startup.cs
@@ -64,7 +64,7 @@ namespace DhruviPatelsBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{area=exists}/{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customers}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
