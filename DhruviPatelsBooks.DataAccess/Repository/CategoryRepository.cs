d7b570f98d19122e63794941819f1282e5c86762
W0831844 <W0831844@ADL-A1201B-27>
2023-11-27 20:28:45 -0500

    create categoryRepository and ICategoryRepository

---
+++ DhruviPatelsBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using DhruviPatelsBooks.DataAccess.Repository.IRepository;
+using DhruviPatelsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DhruviPatelsBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+    }
+}
---
+++ DhruviPatelsBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using DhruviPatelsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DhruviPatelsBooks.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
